dd228f778c65101ea8a6d569da349dc44d3ffa15
Kamlesh364 <patelkamleshpatel364@gmail.com>
2024-05-03 00:45:49 -0700

    add alerts

--- real-time.py
+++ real-time.py
@@ -1,9 +1,8 @@
 import argparse
 import cv2
 import numpy as np
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, flash
 import os
-import threading
 from ultralytics import YOLO
 from torch.cuda import is_available
 
@@ -14,7 +13,6 @@ COLORS = np.random.uniform(0, 255, size=(80, 3)) # random colors for different c
 imgpath = None
 device =  "cuda:0" if is_available() else "cpu"
 video_path = None
-stream = False
 source_link = None
 its_image = False
 PROJECT_PATH = "runs/detect"
@@ -22,6 +20,7 @@ os.makedirs(f"{PROJECT_PATH}", exist_ok=True)
 DOWNLOADS_FOLDER = "uploads"
 os.makedirs(f"{DOWNLOADS_FOLDER}", exist_ok=True)
 app = Flask(__name__)
+app.secret_key = 'ee_267_sjsu$2024'
 
 @app.route("/")
 def hello_world():
@@ -29,95 +28,93 @@ def hello_world():
 
 @app.route("/", methods=["GET", "POST"])
 def predict_img():
-    global imgpath, its_image, video_path, device, source_link
+    global imgpath, its_image, video_path, device, source_link 
+    video_path = None
+    imgpath = None
+    source_link = None
+    its_image = False
     if request.method == "POST":
-        if len(request.files)==0 and len(request.form)==0:
-            return render_template('index.html')
-        if len(request.files)==0 and 'text' in request.form:
-            # video_link = request.form['text']
-            # filepath, is_url = check_source(video_link) # https://drive.google.com/file/d/1OhWnaYeOtMRidwbRpawgoUGhCs2g046h/view?usp=sharing
-            # if is_url:
-            #     filepath = str(check_file(filepath))
-            #     filename = filepath.split('/')[-1]
-            # predict_img.imgpath = filepath
-            # print("uploaded file path: ", filepath)
-            # imgpath, file_extension = (filename.rsplit('.', 1)[0].lower(), filename.rsplit('.', 1)[1].lower())
-            source_link = request.form['text']
-            video_feed()
-            return render_template('index.html')
-        if len(request.files)!=0 and 'file' in request.files:
-            f = request.files['file']
-            basepath = os.path.dirname(__file__)
-            filepath = os.path.join(basepath,DOWNLOADS_FOLDER, f.filename)
-            f.save(filepath)
-            predict_img.imgpath = f.filename
-                                               
-            imgpath, file_extension = (f.filename.rsplit('.', 1)[0].lower(), f.filename.rsplit('.', 1)[1].lower()) 
-            
-        if os.path.exists(f"{PROJECT_PATH}"):
-            os.makedirs(f"{PROJECT_PATH}/{imgpath}", exist_ok=True)
-        else:
-            os.makedirs(f"runs", exist_ok=True)
-            os.makedirs(f"{PROJECT_PATH}")
-            os.makedirs(f"{PROJECT_PATH}/{imgpath}")
+        if request.files:
+            if len(request.files["file"].filename)==0:
+                if 'text' in request.form:
+                    source_link = request.form['text']
+                    video_feed()
+                    return render_template('index.html')
+                else:
+                    flash("Please upload a file first to proceed!")
+                    return render_template('index.html')
+            else:
+                f = request.files['file']
+                basepath = os.path.dirname(__file__)
+                filepath = os.path.join(basepath,DOWNLOADS_FOLDER, f.filename)
+                f.save(filepath)
+                predict_img.imgpath = f.filename
+                                                
+                imgpath, file_extension = (f.filename.rsplit('.', 1)[0].lower(), f.filename.rsplit('.', 1)[1].lower())
 
-        if file_extension in IMG_FORMATS:
-            its_image = True
-            frame = cv2.imread(filepath)
-            # Inference
-            results = MODEL.predict(
-                frame, show=False, verbose=False, save=False, device=device, conf=0.5
-            )
+                if os.path.exists(f"{PROJECT_PATH}"):
+                    os.makedirs(f"{PROJECT_PATH}/{imgpath}", exist_ok=True)
+                else:
+                    os.makedirs(f"runs", exist_ok=True)
+                    os.makedirs(f"{PROJECT_PATH}")
+                    os.makedirs(f"{PROJECT_PATH}/{imgpath}")
 
-            # Check if robot is detected
-            if results[0].boxes.cpu().numpy().xyxy.shape[0] != 0:
-                # Show results on image
-                boxes = results[0].boxes.cpu().numpy().xyxy.astype(int)
-                labels = results[0].boxes.cpu().numpy().cls
-                conf = results[0].boxes.cpu().numpy().conf
-                for box, label, conf in zip(boxes, labels, conf):
-                    x1, y1, x2, y2 = box
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), COLORS[int(label)], 4)
-                    cv2.putText(
-                        frame,
-                        MODEL.names[int(label)] + ": " + str(round(conf, 2)),
-                        (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        4,
-                        COLORS[int(label)],
-                        4,
+                if file_extension in IMG_FORMATS:
+                    its_image = True
+                    frame = cv2.imread(filepath)
+                    # Inference
+                    results = MODEL.predict(
+                        frame, show=False, verbose=False, save=False, device=device, conf=0.5
                     )
-                cv2.imwrite(f"{PROJECT_PATH}/{imgpath}/output.jpg", frame)
-                image_feed()
-                return render_template('index.html')  
-        
-        elif file_extension in VID_FORMATS:
-            its_image = False
-            video_path = filepath
-            video_feed()
-            return render_template('index.html')
+
+                    # Check if robot is detected
+                    if results[0].boxes.cpu().numpy().xyxy.shape[0] != 0:
+                        # Show results on image
+                        boxes = results[0].boxes.cpu().numpy().xyxy.astype(int)
+                        labels = results[0].boxes.cpu().numpy().cls
+                        conf = results[0].boxes.cpu().numpy().conf
+                        for box, label, conf in zip(boxes, labels, conf):
+                            x1, y1, x2, y2 = box
+                            cv2.rectangle(frame, (x1, y1), (x2, y2), COLORS[int(label)], 4)
+                            cv2.putText(
+                                frame,
+                                MODEL.names[int(label)] + ": " + str(round(conf, 2)),
+                                (x1, y1 - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                4,
+                                COLORS[int(label)],
+                                4,
+                            )
+                        cv2.imwrite(f"{PROJECT_PATH}/{imgpath}/output.jpg", frame)
+                        image_feed()
+                        return render_template('index.html')  
+                
+                elif file_extension in VID_FORMATS:
+                    its_image = False
+                    video_path = filepath
+                    video_feed()
+                    return render_template('index.html')
+
+        if request.form:
+            if len(request.form["text"])==0:
+                flash("Please provide a link first to proceed!")
+                return render_template('index.html')
+            else:
+                if 'text' in request.form:
+                    source_link = request.form['text']
+                    video_feed()
+                    return render_template('index.html')
 
     return render_template('index.html')
 
 
 def get_video_frame():
-    global imgpath, video_path, device, source_link
-
-    if video_path==None and source_link==None:
-        mp4_files = f"{PROJECT_PATH}/{imgpath}/output.mp4"
-        video = cv2.VideoCapture(mp4_files)  # detected video path
-        while True:
-            success, image = video.read()
-            if not success:
-                break
-            _, jpeg = cv2.imencode('.jpg', image) 
-        
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
-        video.release()
+    global video_path, device, source_link
 
     if video_path!=None and source_link==None:
-        cap = cv2.VideoCapture(video_path)
+        pth = video_path
+        video_path = None
+        cap = cv2.VideoCapture(pth)
         # Initialize variables
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
@@ -166,11 +163,13 @@ def get_video_frame():
         cap.release()
         out.release()
     
-    if source_link!=None:
+    elif source_link!=None:
         print("source_link: ", source_link)
         # Inference
+        pth = source_link
+        source_link = None
         results = MODEL.predict(
-            source_link, show=False, verbose=False, save=False, device=device, conf=0.5, stream=True
+            pth, show=False, verbose=False, save=False, device=device, conf=0.5, stream=True
         )
         while True:
             for result in results:
@@ -206,6 +205,8 @@ def get_image_frame():
     else: # clear the image
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + b'\r\n\r\n')
+    imgpath = None
+    its_image = False
 
 @app.route("/image_feed")
 def image_feed():
