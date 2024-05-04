82b23d8b265a3b51d8ea870e0c9173fc0a838761
Kamlesh Kumar <patelkamleshpatel364@gmail.com>
2024-05-04 10:13:54 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 sudo apt update
 sudo apt install -y python3-pip
-git clone https://github.com/Kamlesh364/Object-Detection-Web-App.git
-cd Object-Detection-Web-App
 export PATH="$PATH:~/.local/bin"
 pip install -r requirements.txt
 sudo apt install -y python3-opencv
